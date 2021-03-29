44d69f1532ff063130361b4de4734f696c14f851
Mathieu Vermeire <mathieu.vermeire@student.howest.be>
2021-03-29 17:10:06 +0200

    push

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css'
 
 export async function getStaticProps() {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_ARTICLES}/articles`);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_ARTICLES}articles`);
 	const data = await res.json();
 
 	return {
